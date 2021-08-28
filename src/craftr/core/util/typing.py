d8299ea4fb821b07c4707f5bb78b8825fafe915d
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-08-28 22:25:05 +0200

    filter out type vars in unpack_type_hints()

--- src/craftr/core/util/typing.py
+++ src/craftr/core/util/typing.py
@@ -3,16 +3,20 @@ import typing as t
 import typing_extensions as te
 
 
+def _filter_typevars(args: t.Iterable[t.Any]) -> t.List[t.Any]:
+  return [x for x in args if not isinstance(x, t.TypeVar)]
+
+
 def unpack_type_hint(hint: t.Any) -> t.Tuple[t.Optional[t.Any], t.List[t.Any]]:
   """
   Unpacks a type hint into it's origin type and parameters.
   """
 
   if isinstance(hint, te._AnnotatedAlias):  # type: ignore
-    return te.Annotated, list((hint.__origin__,) + hint.__metadata__)  # type: ignore
+    return te.Annotated, _filter_typevars((hint.__origin__,) + hint.__metadata__)  # type: ignore
 
   if isinstance(hint, t._GenericAlias):  # type: ignore
-    return hint.__origin__, list(hint.__args__)
+    return hint.__origin__, _filter_typevars(hint.__args__)
 
   if hasattr(t, '_SpecialGenericAlias') and isinstance(hint, t._SpecialGenericAlias):  # type: ignore
     return hint.__origin__, []
